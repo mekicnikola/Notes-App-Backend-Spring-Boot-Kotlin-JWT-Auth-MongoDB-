2bee0a035a19f8ce04815d6e77dcf262e9367a77
nikola <nikolamekic@gmail.com>
2025-04-28 17:42:11 +0200

    Fix Status Controller Annotation

--- src/main/kotlin/com/nikolamekic/notes/controllers/StatusController.kt
+++ src/main/kotlin/com/nikolamekic/notes/controllers/StatusController.kt
@@ -1,11 +1,14 @@
 package com.nikolamekic.notes.controllers
 
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/")
 class StatusController {
+
+    @GetMapping
     fun getStatus(): String {
         return "Everything works!"
     }
